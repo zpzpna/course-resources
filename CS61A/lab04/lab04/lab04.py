f14fdbe592e7fcf218f09335456c3d0993905025
Wang zhipeng <zpwangsena@gmail.com>
2023-08-07 23:35:52 +0800

    Finish cs61a-lab04
    	- must + list in flatten of list
    
    	- abstract l1,l2

---
+++ CS61A/lab04/lab04/lab04.py
@@ -0,0 +1,173 @@
+def flatten(s):
+    """Returns a flattened version of list s.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]     # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x # Ensure x is not mutated
+    [1, [2, 3], 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, 1]], 1, [1, 1]]
+    >>> x = [[1, [2, 3], [4, [5, 6]]]]
+    >>> flatten(x)
+    [1, 2, 3, 4, 5, 6]
+    >>> x
+    [[1, [2, 3], [4, [5, 6]]]]
+    >>> x = [[1, [1, [1, [1, 1, [1, 1, [1]]]], 1]]]
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, [1, [1, 1, [1, 1, [1]]]], 1]]]
+    """
+    "*** YOUR CODE HERE ***"
+    #iterable way
+    # source, result, flag = s, [], True
+    # while True:
+        # for elem in source:
+            # if list == type(elem):
+                # result, flag = result + elem, False
+            # else:
+                # result += [elem]
+        # if flag == True:
+            # break
+        # else:
+            # source, result, flag = result, [], True
+    # return result
+    
+    #recursive way
+    result = []
+    for elem in s:
+        if list == type(elem):
+            result += flatten(elem)
+        else:
+            result += [elem]
+    return result
+        
+    
+
+
+def my_map(fn, seq):
+    """Applies fn onto each element in seq and returns a list.
+    >>> my_map(lambda x: x*x, [1, 2, 3])
+    [1, 4, 9]
+    >>> my_map(lambda x: abs(x), [1, -1, 5, 3, 0])
+    [1, 1, 5, 3, 0]
+    >>> my_map(lambda x: print(x), ['cs61a', 'summer', '2023'])
+    cs61a
+    summer
+    2023
+    [None, None, None]
+    """
+    return [fn(elem) for elem in seq]
+
+
+def my_filter(pred, seq):
+    """Keeps elements in seq only if they satisfy pred.
+    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
+    [2, 4]
+    >>> my_filter(lambda x: (x + 5) % 3 == 0, [1, 2, 3, 4, 5])
+    [1, 4]
+    >>> my_filter(lambda x: print(x), [1, 2, 3, 4, 5])
+    1
+    2
+    3
+    4
+    5
+    []
+    >>> my_filter(lambda x: max(5, x) == 5, [1, 2, 3, 4, 5, 6, 7])
+    [1, 2, 3, 4, 5]
+    """
+    return [elem for elem in seq if pred(elem)]
+
+
+def my_reduce(combiner, seq):
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    reduced = seq[0]
+    for elem in seq[1:]:
+        reduced = combiner(reduced,elem)
+    return reduced
+        
+
+
+def my_map_syntax_check():
+    """Check that your two_of_three code consists of nothing but a return statement.
+
+    >>> # You aren't expected to understand the code of this test.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(my_map)).body[0].body]
+    ['Expr', 'Return']
+    """
+    # You don't need to edit this function. It's just here to check your work.
+
+
+def my_filter_syntax_check():
+    """Check that your two_of_three code consists of nothing but a return statement.
+
+    >>> # You aren't expected to understand the code of this test.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(my_filter)).body[0].body]
+    ['Expr', 'Return']
+    """
+    # You don't need to edit this function. It's just here to check your work.
+
+#important answers about abstraction in rescursion method
+def merge(lst1, lst2):
+    """Merges two sorted lists.
+
+    >>> s1 = [1, 3, 5]
+    >>> s2 = [2, 4, 6]
+    >>> merge(s1, s2)
+    [1, 2, 3, 4, 5, 6]
+    >>> s1
+    [1, 3, 5]
+    >>> s2
+    [2, 4, 6]
+    >>> merge([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    >>> merge([2, 3, 4], [2, 4, 6])
+    [2, 2, 3, 4, 4, 6]
+    """
+    "*** YOUR CODE HERE ***"
+    def find_min(lst1,lst2):
+        if lst1[0]<=lst2[0]:
+            return lst1,lst2
+        else:
+            return lst2,lst1
+    # if lst1 == []:
+        # return lst2
+    # elif lst2 == []:
+        # return lst1
+    if not lst1 or not lst2:
+        return lst1+lst2
+    else:
+        list1,list2 = find_min(lst1,lst2)
+        return [list1[0]]+merge(list1[1:],list2)
+
+def count_palindromes(L):
+    """The number of palindromic words in the sequence of strings
+    L (ignoring case).
+
+    >>> count_palindromes(("Acme", "Madam", "Pivot", "Pip"))
+    2
+    """
+    return len([ word for word in L if word.lower() == (word.lower())[::-1]])
